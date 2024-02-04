2332f75b404ea03a752c1c561473c3f922dac7a0
realize1103 <realize1103@gmail.com>
2024-02-04 20:37:39 +0100

    feat: Defining Routes

--- learn-nextjs14/app/about-us/page.tsx
+++ learn-nextjs14/app/about-us/page.tsx
@@ -1,3 +1,9 @@
+import Avatar from "./components/avatar";
+
 export default function AboutUs(){
-    return <h1>About us!</h1>
+    return (
+        <h1>
+            About us!<Avatar/>
+        </h1>
+    );
 }
