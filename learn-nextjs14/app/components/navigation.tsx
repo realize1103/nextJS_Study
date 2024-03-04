036492cb16a96bb18c061326b8a3e218f5c10a4a
realize1103 <realize1103@gmail.com>
2024-03-04 17:50:04 +0100

    feat: add Layout example

--- learn-nextjs14/app/components/navigation.tsx
+++ learn-nextjs14/app/components/navigation.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import AboutUs from "../about-us/page";
 import { usePathname } from "next/navigation";
--- learn-nextjs14/app/page.tsx
+++ learn-nextjs14/app/page.tsx
@@ -1,8 +1,5 @@
-import Navigation from "./components/navigation";
-
 export default function Tomato() {
     return <div>
-        <Navigation />
         <h1>Welcome</h1>
     </div>;
 }
