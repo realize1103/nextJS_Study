13f3551f6ff87632745a151dae18432fa25bac7a
realize1103 <realize1103@gmail.com>
2024-03-09 10:21:38 +0100

    feat: as-is react fetching style

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -1,10 +1,23 @@
-export const metadata = {
-  title: 'Home',
-};
+"use client"
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//   title: 'Home',
+// };
 
 export default function Page() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState();
+    const getMovies = async () => {
+        const res = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+        const movies = await res.json();
+        setMovies(movies);
+        setIsLoading(false);
+    };
+    useEffect(() => { getMovies(); }, []);
+
     return <div>
-        <h1>Welcome</h1>
+        {isLoading ? "Loading...": JSON.stringify(movies)}
     </div>;
 }
 
