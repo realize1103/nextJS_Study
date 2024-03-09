4dad6d0187fa2519493843d23e42f2192862e7a3
realize1103 <realize1103@gmail.com>
2024-03-09 19:02:43 +0100

    feat: ServerSide Fetch

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -1,23 +1,21 @@
-"use client"
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: 'Home',
+};
 
-// export const metadata = {
-//   title: 'Home',
-// };
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState();
-    const getMovies = async () => {
-        const res = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
-        const movies = await res.json();
-        setMovies(movies);
-        setIsLoading(false);
-    };
-    useEffect(() => { getMovies(); }, []);
+async function getMovies() {
+    const res = await fetch(URL);
+    const data = await res.json();
+    return data;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
+    console.log(movies);
 
     return <div>
-        {isLoading ? "Loading...": JSON.stringify(movies)}
+        {JSON.stringify(movies)}
     </div>;
 }
 
